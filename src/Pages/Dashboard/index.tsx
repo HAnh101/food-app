4c34dfa0271a777db3d5180a700e685fec1469a2
MiaYinel <103024412+HAnh101@users.noreply.github.com>
2023-09-12 01:42:07 +0700

    UI-order-item

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,15 +1,41 @@
-import { Typography } from 'antd';
+import { Button, Drawer, Typography } from 'antd';
 import FoodList from '../../Components/FoodList';
+import { useState } from 'react';
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import OrderItem from '../../Components/OrderItem';
 
 function Dashboard() {
+  const [isOpen, setIsOpen] = useState(false)
   const onSelectDish = () => {
 
   }
   return (
     <main>
-      <Typography.Title level={4}>Choose Dishes</Typography.Title>
+      <div className='flex justify-between'>
+        <Typography.Title level={4}>Choose Dishes</Typography.Title>
+        <Button
+          icon={!isOpen ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          onClick={() => setIsOpen(!isOpen)}
+          // style={{
+          //   fontSize: "16px",
+          //   width: 64,
+          //   height: 64,
+          // }}
+        />
+      </div>
 
       <FoodList onClick={onSelectDish} type="view" />
+      <Drawer
+        title={"New order"}
+        // width={720}
+        width={window.screen.availWidth < 400 ? 320 : 520}
+        onClose={() => setIsOpen(false)}
+        open={isOpen}
+        closeIcon={null}
+        bodyStyle={{ paddingBottom: 80 }}
+      >
+        <OrderItem />
+      </Drawer>
     </main>
   );
 }
